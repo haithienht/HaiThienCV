e06ff16c5f06b37286fb38339768089d94504e07
nphthien <nphthien@cmc.com.vn>
2021-02-26 17:26:43 +0700

    update data showing function

--- js/data.js
+++ js/data.js
@@ -1,4 +1,21 @@
 var mydata = {
+    "personal_info": {
+        "name": "NGUYEN PHAN HAI THIEN",
+        "birthday": "09/18/1993",
+        "email": "haithiennpht@gmail.com",
+        "phone": "0903391712",
+        "hobbies": [
+            "Badminton",
+            "Music",
+            "Guitar",
+            "Making video games",
+            "Video games",
+        ],
+        "experience": {
+            "Software Engineer": "~8 months exp",
+            "Japanese Teacher": ">2 yrs exp"
+        },
+    },
     "skills": {
         "Technical Skills": {
             "Programing Languages": [
@@ -63,6 +80,86 @@ var mydata = {
             "English (Reading 4/5 - Speaking 2/5)",
             "Japanese (Reading 4/5 - Speaking 3/5)",
         ],
-    }
-
+    },
+    "education": {
+        "International Programer": {
+            "date_from": "04/2018",
+            "date_to": "04/2020",
+            "place": "University of Pedagogy Ho Chi Minh City",
+            "detail": "Academic transcript/scores: <a href='img/img002.jpg' target='_blank'>Semester 1</a> <a href='img/img003.jpg' target='_blank'>Semester 2</a>",
+        },
+        "Japanese Bachelor": {
+            "date_from": "08/2011",
+            "date_to": "08/2015",
+            "place": "University of Pedagogy Ho Chi Minh City",
+            "detail": "",
+        },
+    },
+    "experience": [
+        {
+            "title": "Software Engineer at CMC Global",
+            "date_from": "06/2020",
+            "date_to": "02/2021",
+            "main": {
+                "Participated in projects using": [
+                    "Salesforce cloud – Apex – Lightning Web Components – Javascript – Powershell - VBA (~4 months)",
+                    "C# .NET – Window Form – Geometry – Azure/MSSQL (~ 2 months)",
+                    "VB .NET - MSSQL (~1 months)",
+                    "Java – Sping Boot Rest API – PostgreSql - MessageQueue (~ 1 months)",
+                ],
+                "Projects": [
+                    "[Salesforce][Maintainance and new features dev][Japanese] Agriculture and Forestry’s Application/Petition Management System",
+                    "[C# .NET][Create from scratch][Japanese] Data Process/Import with maps data",
+                    "[VB.NET][ Maintainance/Fix bugs][Japanese] Funding Application/Petition Management System",
+                    "[Java][Research] Cross section’s image Management System",
+                ],
+                "Achieved": [
+                    "Gitlab, git working flow, task management with JIRA",
+                    "Self-learning new techs.",
+                    "Know more app basic working flow.",
+                    "Improve IT/Development’s vocabulary.",
+                ],
+            },
+            "more": {}
+        },
+        {
+            "title": "Final Java Project: PC Part E-commerce WebApp",
+            "date_from": "01/2020",
+            "date_to": "04/2020",
+            "main": {
+                "References": [
+                    "<a href='https://github.com/HaiNguyenPV12/HTDShop/graphs/contributors'>Github</a>",
+                    "<a href='data/HTDPCShop.sql'>SQL DATA</a>",
+                ],
+                "Details": [
+                    "Customers can see or search for product detail, register and login for purchased history, make orders...",
+                    "For shop owner, provide administrative tool (categories/products/orders/members/staff/roles…).",
+                ],
+                "Achieved": [
+                    "More programing knowledge of Java EE.",
+                    "Self-learning Java framework/engine (Spring MVC, Thymeleaf).",
+                    "Use Maven to manage dependencies.",
+                    "Glassfish server deployment.",
+                ],
+            },
+            "more": {
+                "Teamsize": "3 persons",
+                "Role": "Team leader",
+                "Programs": {
+                    "Netbeans 8.2": "Create base project, debug, deploy project to Glassfish server.",
+                    "Visual Studio Code": "Main coding tool.",
+                    "SQL Server Management Studio": "SQL Server Manager.",
+                    "Github": "Version control and team management.",
+                },
+                "Technologies":[
+                    "Java EE (deploy on Glassfish Server)",
+                    "Database MS SQL Server",
+                    "Java Persistent API",
+                    "Thymeleaf template engine",
+                    "JQuery",
+                    "Bootstrap",
+                ]
+            }
+        },
+    ]
 }
--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,44 @@
 	* Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
 	*/
 (function ($) {
-	function generateData($data) {
+
+	function generateData(data) {
+		// PERSONAL DATA
+		$("#name").html(data["personal_info"]["name"])
+		$("#email").html("<a href='mailto:" + data["personal_info"]["email"] + "' class='text-light'>" + data["personal_info"]["email"] + "</a>")
+		$("#phone").html(data["personal_info"]["phone"])
+		let hobbies = ""
+		for (let hobby in data["personal_info"]["hobbies"]) {
+			hobbies += '<span class="badge badge-secondary">' + data["personal_info"]["hobbies"][hobby] + '</span>'
+		}
+		$("#hobbies").html(hobbies)
+		let shortExperince = ""
+		for (let exp in data["personal_info"]["experience"]) {
+			shortExperince += '<span class="badge badge-pill badge-secondary">' + exp + ' <sup><i>' + data["personal_info"]["experience"][exp] + '</i></sup></span>'
+		}
+		$("#experience_short").html(shortExperince)
+
+
+		// SKILL DATA
 		let techSkills = $("#tech_skills")
 		let otherSkills = $("#other_skills")
 		techSkills.html("")
 		otherSkills.html("")
-		for (const key in $data["skills"]) {
+		for (const key in data["skills"]) {
 			let html = '<h3 class="text-info">' + key.toUpperCase() + '</h3>'
 			let skillHtml = ''
-			if (!Array.isArray($data["skills"][key])) {
-				for (const skillitem in $data["skills"][key]) {
+			if (!Array.isArray(data["skills"][key])) {
+				for (const skillitem in data["skills"][key]) {
 					skillHtml += "<h5>" + skillitem + "</h5>"
 					skillHtml += "<ul>"
-					$data["skills"][key][skillitem].forEach(element => {
+					data["skills"][key][skillitem].forEach(element => {
 						skillHtml += '<li>' + element + '</li>'
 					});
 					skillHtml += "</ul>"
 				}
 			} else {
 				skillHtml += "<ul>"
-				$data["skills"][key].forEach(element => {
+				data["skills"][key].forEach(element => {
 					skillHtml += '<li>' + element + '</li>'
 				});
 				skillHtml += "</ul>"
@@ -31,6 +49,49 @@
 			html += skillHtml
 			if (key == "Technical Skills") { techSkills.append(html) } else { otherSkills.append(html) }
 		}
+
+		// EDUCATION DATA
+		var eduBadge = function (type) {
+			let html = '<div class="col-auto text-center flex-column d-none d-sm-flex"><div class="row h-50">'
+				+ '<div class="col ' + ((type === "first") ? "" : " border-right border-light") + '">&nbsp;</div>'
+				+ '<div class="col">&nbsp;</div></div><h5 class="m-2"><span class="badge badge-pill bg-light border border-info">&nbsp;</span></h5><div class="row h-50">'
+				+ '<div class="col ' + ((type === "last") ? "" : " border-right border-light") + '">&nbsp;</div>'
+				+ '<div class="col">&nbsp;</div></div></div >';
+			return html;
+		}
+		var eduContent = function (dateFrom, dateTo, title, place, detail) {
+			let html = '<div class="col py-2 text-dark"><div class="card border-info shadow"><div class="card-body"><div class="float-right">'
+				+ '<span class="badge badge-info">' + dateFrom + '</span>'
+				+ '<i class="fas fa-angle-right text-info"></i>'
+				+ '<span class="badge badge-info">' + dateTo + '</span>'
+				+ '</div>'
+				+ '<h4 class="card-title text-info">' + title + '</h4>'
+				+ '<span class="card-text">'
+				+ '<div><b>At: </b><span>' + place + '</span></div>'
+				+ (detail ? ('<div><b>Details: </b><span class="card-text text-dark">' + detail + '</span></div>') : "")
+				+ '</span></div></div></div>'
+			return html;
+		}
+
+		let eduHtml = '';
+		let isFirst = true;
+		let lastKey = Object.keys(data["education"])[Object.keys(data["education"]).length - 1]
+
+		for (const key in data["education"]) {
+			eduHtml += '<div class="row">';
+			if (isFirst) {
+				eduHtml += eduBadge('first')
+				isFirst = false
+			} else if (key == lastKey) {
+				eduHtml += eduBadge('last')
+			} else {
+				eduHtml += eduBadge('')
+			}
+			eduHtml += eduContent(data["education"][key]["date_from"], data["education"][key]["date_to"], key, data["education"][key]["place"], data["education"][key]["detail"])
+			eduHtml += '</div>'
+		}
+
+		$("#education").html(eduHtml);
 	}
 	generateData(mydata);
 
